675821e8fc967a87d7071ec87de2acd8ed837192
Sttand <wandersonamarantesousa@gmail.com>
2025-07-06 02:37:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from rembg import remove
 from io import BytesIO
 
 app = FastAPI()
+
+# Ativando CORS para permitir acesso do GitHub Pages
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Você pode restringir isso para o domínio do GitHub Pages depois
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/remover-fundo")
 async def remover_fundo(file: UploadFile = File(...)):
